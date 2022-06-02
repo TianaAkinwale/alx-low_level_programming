41ac72df6784c866a6537d2b828e13851f73bf77
TianaAkinwale <tianorway@gmail.com>
2022-06-02 12:28:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <unstdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Desc: printsAand that piece of art is useful" - Dora Korpar, 2015-10-19,"
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
-			", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar,
+			2015-10-19\n", 59);
 	return (1);
 }
