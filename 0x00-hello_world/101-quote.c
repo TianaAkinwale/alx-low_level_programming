35a2401783830ee77e814bcf4359b08659f1e90e
TianaAkinwale <tianorway@gmail.com>
2022-06-02 12:37:14 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  *
  * Desc: printsAand that piece of art is useful" - Dora Korpar, 2015-10-19,"
  *
- * Return: Always 1 (Error)
+ * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
