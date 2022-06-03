dd17782f6a0d12a53182367d74bc11b13a7c0a8d
TianaAkinwale <tianorway@gmail.com>
2022-06-03 02:40:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ *ddesc:prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success/corect)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 48 ; i <= 57; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
+}
