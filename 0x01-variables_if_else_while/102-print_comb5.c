2fbbb7f455a2cd7baa195a7fe4841df9df2c2724
TianaAkinwale <tianorway@gmail.com>
2022-06-03 04:48:03 -0700

    comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,9 @@
 int main(void)
 {
 	int first_nums;
+
 	int second_nums;
+
 	for (first_nums = 0; first_nums < 100 ; first_nums++)
 	{
 		for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
