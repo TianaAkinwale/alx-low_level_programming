51d4ed100a7330a887a0ae4040b460d00c07e435
TianaAkinwale <tianorway@gmail.com>
2022-06-07 14:16:50 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,14 @@
  * Return: integer
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
 		return (n);
-	}
-	else if (n < 0)
-	{
-		return (-n);
 	}
 	else
 	{
-		return (n);
+		return (-n);
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,3 +8,5 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+
+#endif
