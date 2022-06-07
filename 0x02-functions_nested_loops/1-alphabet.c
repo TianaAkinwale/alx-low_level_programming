740645123e1b0c98369d67dbfcaa1a0bf37ef7ef
TianaAkinwale <tianorway@gmail.com>
2022-06-07 09:23:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function prototype
+ *
+ * desc-prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+		_putchar('a');
+	}
+	{
+		_putchar('\n');
+	}
+}
