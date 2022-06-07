6345537a145736c717c1a8cc22a1bef6cfb4f052
TianaAkinwale <tianorway@gmail.com>
2022-06-07 09:29:51 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
 
 void print_alphabet(void)
 {
-	char a;
+	char alphabet;
 
-	for(a = 'a'; a <= 'z'; a++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar('a');
+		_putchar(alphabet);
 	}
 	{
 		_putchar('\n');
