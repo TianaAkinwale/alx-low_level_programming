c70af3456d011f02e029856de7229fdb3b94595f
TianaAkinwale <tianorway@gmail.com>
2022-06-07 12:50:48 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  *
- * n: an ascii character
+ * n: carrier variable
  *
- * Return: 1
+ * Return: 1 if n>0, 0 if n==0, -1 if n<0
  */
 
 int print_sign(int n)
@@ -13,21 +13,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\0');
 
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar('\0');
 
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('\n');
 
 		return (-1);
 	}
