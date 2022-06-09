a14ef1752a8a6138a574a722e36a06969f9c1dec
TianaAkinwale <tianorway@gmail.com>
2022-06-09 04:32:39 -0700

    mainc

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer
+ * is negative or positive
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative_(i);
+
+	return (0);
+}
