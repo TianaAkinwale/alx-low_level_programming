119ea2e7fd11f243be25179ac4a1d5f92206d187
TianaAkinwale <tianorway@gmail.com>
2022-07-04 13:05:37 -0700

    dogsss

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,16 +7,19 @@
  * @name: member
  * @age: member
  * @owner: member
+ * Description: This struct is for dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL) /* validate if d initiated correctly */
 	{
-		return ();
+		return;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
