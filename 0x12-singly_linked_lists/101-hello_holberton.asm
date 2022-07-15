d0441a9eaf420e2f632933246b43a41ada997600
TianaAkinwale <tianorway@gmail.com>
2022-07-14 18:12:00 -0700

    chamnges to hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,21 @@
-ection .data
-msg:	 db "Hello, Holberton", 0
-fmt:	 db "%s", 10, 0
+extern printf
 
-	section .text
-	global main
+section .text
+   global main
+
 main:
-	push rbp
-	mov rdi,fmt
-	mov rsi,msg
-	mov rax,0
-	call printf
+   push rbp
 
-	pop rbp
-	mov rax,0
-	ret
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
